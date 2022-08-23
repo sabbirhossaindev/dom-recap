8a0a5d0333eba527fd421dc64740907afcdc6d17
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-24 01:20:08 +0600

    dome recap

---
+++ validation.js
@@ -0,0 +1,16 @@
+const number = 23;
+// string, number, boolean
+if(typeof number === 'number'){
+    console.log('value is a number')
+}
+else{
+    console.log('value is not a number')
+}
+
+const numbers = [3, 12, 78, 90];
+const student = {name: 'Rongila Rafsan', job: 'khawadawa kora'}
+// console.log(Array.isArray(numbers));
+// isNan() ulta vava kaj kora..
+// undefined...
+console.log(Array.isArray(number));
+console.log(typeof student);
